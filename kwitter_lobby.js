3796f27f9e42fac6080163eda448fca6d7c5e853
Razdy <76704902+Razdy@users.noreply.github.com>
2021-07-21 16:15:33 -0400

    Add files via upload

---
+++ kwitter_lobby.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAlSuIjo-iYtce99nOZpoQBONuvRtrbwtw",
+    authDomain: "let-s-chat-app-e58ff.firebaseapp.com",
+    databaseURL: "https://let-s-chat-app-e58ff-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-app-e58ff",
+    storageBucket: "let-s-chat-app-e58ff.appspot.com",
+    messagingSenderId: "884262414177",
+    appId: "1:884262414177:web:1ef770e9bcea856fa84720"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
